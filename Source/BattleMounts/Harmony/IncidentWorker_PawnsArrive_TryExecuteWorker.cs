0878d60edd2569e3156c6bb672889785f822f0a9
Roel <roelheirman@gmail.com>
2017-12-08 19:33:31 +0100

    tried mounting after spawn

---
+++ Source/BattleMounts/Harmony/IncidentWorker_PawnsArrive_TryExecuteWorker.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Battlemounts.Harmony
+{
+    [HarmonyPatch(typeof(IncidentWorker_PawnsArrive), "TryExecuteWorker")]
+    static class IncidentWorker_PawnsArrive_TryExecuteWorker
+    {
+        static void Postfix(IncidentWorker_PawnsArrive __instance, ref IncidentParms parms)
+        {
+            Map map = (Map) parms.target;
+
+
+            foreach (Thing thing in from thing in map.spawnedThings
+                                    where (thing is Pawn && (Pawn)thing.Faction != null && (Pawn)thing.Faction == )
+                                    select thing) ;
+        }
+    }
+}
