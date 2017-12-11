d5d8834275a38e61146de90ccc06e1c0336b2a95
Roel <roelheirman@gmail.com>
2017-12-11 20:31:25 +0100

    added mounts for enemy raids, ambushes not working yet

--- Source/BattleMounts/Harmony/IncidentWorker_PawnsArrive_TryExecuteWorker.cs
+++ Source/BattleMounts/Harmony/IncidentWorker_PawnsArrive_TryExecuteWorker.cs
@@ -1,24 +1,46 @@
-﻿using Harmony;
+﻿using Battlemounts.Utilities;
+using BattleMounts;
+using BattleMounts.Jobs;
+using GiddyUpCore.Storage;
+using GiddyUpCore.Utilities;
+using Harmony;
 using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using Verse;
+using Verse.AI;
 
 namespace Battlemounts.Harmony
 {
-    [HarmonyPatch(typeof(IncidentWorker_PawnsArrive), "TryExecuteWorker")]
-    static class IncidentWorker_PawnsArrive_TryExecuteWorker
+    [HarmonyPatch(typeof(IncidentWorker_Raid), "TryExecuteWorker")]
+    static class IncidentWorker_Raid_TryExecuteWorker
     {
-        static void Postfix(IncidentWorker_PawnsArrive __instance, ref IncidentParms parms)
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            Map map = (Map) parms.target;
+            var instructionsList = new List<CodeInstruction>(instructions);
 
+            for (var i = 0; i < instructionsList.Count; i++)
+            {
+                CodeInstruction instruction = instructionsList[i];
+                yield return instruction;
+                //Log.Message(instructionsList[i].opcode.ToString());
+                //Log.Message(instructionsList[i].operand as String);
+                
+                if (instruction.operand as String == "F0") //find operand
+                {
+                    //Start of injection
+                    yield return new CodeInstruction(OpCodes.Ldloc_2);//load generated pawns as parameter
+                    yield return new CodeInstruction(OpCodes.Ldarg_1);//load incidentparms as parameter
+                    yield return new CodeInstruction(OpCodes.Call, typeof(NPCMountUtility).GetMethod("mountAnimals"));//Injected code
+                    //yield return new CodeInstruction(OpCodes.Stloc_2);
+                }
+                
+            }
 
-            foreach (Thing thing in from thing in map.spawnedThings
-                                    where (thing is Pawn && (Pawn)thing.Faction != null && (Pawn)thing.Faction == )
-                                    select thing) ;
         }
+    
     }
 }
---
+++ Source/BattleMounts/Utilities/NPCMountUtility.cs
@@ -0,0 +1,110 @@
+﻿using BattleMounts;
+using BattleMounts.Jobs;
+using GiddyUpCore.Storage;
+using GiddyUpCore.Utilities;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace Battlemounts.Utilities
+{
+    class NPCMountUtility
+    {
+        public static void mountAnimals(List<Pawn> list, IncidentParms parms)
+        {
+            if(list == null)
+            {
+                Log.Message("list is null");
+            }
+
+            if (list.Count == 0 || parms.raidArrivalMode != PawnsArriveMode.EdgeWalkIn)
+            {
+                return;
+            }
+
+
+            Map map = (Map)parms.target;
+            List<Pawn> animals = new List<Pawn>();
+            int enemyMountChance = 0;
+            if (parms.faction.def == FactionDefOf.Tribe)
+            {
+                enemyMountChance = Base.enemyMountChanceTribal;
+            }
+            else if (parms.faction.def != FactionDefOf.Spacer && parms.faction.def != FactionDefOf.SpacerHostile && parms.faction.def != FactionDefOf.Mechanoid)
+            {
+                enemyMountChance = Base.enemyMountChance;
+            }
+            else
+            {
+                return;
+            }
+
+            Random rand = new Random(DateTime.Now.Millisecond);
+            foreach (Pawn pawn in list)
+            {
+                //TODO add chance
+                int rndInt = rand.Next(1, 100);
+                Log.Message("rndInt: " + rndInt);
+                Log.Message("mountChance: " + enemyMountChance);
+                if (enemyMountChance <= rndInt)
+                {
+                    continue;
+                }
+
+                PawnKindDef pawnKindDef = (from a in map.Biome.AllWildAnimals
+                                           where map.mapTemperature.SeasonAcceptableFor(a.race) && isMountable(a.defName)
+                                           select a).RandomElementByWeight((PawnKindDef def) => map.Biome.CommonalityOfAnimal(def));
+
+                if (pawnKindDef == null)
+                {
+                    Log.Error("No spawnable animals right now.");
+                    return;
+                }
+
+                Pawn animal = PawnGenerator.GeneratePawn(pawnKindDef, parms.faction);
+
+                IntVec3 loc = CellFinder.RandomClosewalkCellNear(parms.spawnCenter, map, 8, null);
+                GenSpawn.Spawn(animal, loc, map, parms.spawnRotation, false);
+
+                ExtendedPawnData pawnData = GiddyUpCore.Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(pawn);
+
+                if (pawnData == null)
+                {
+                    Log.Message("pawndata is null");
+                }
+                pawnData.mount = animal;
+                TextureUtility.setDrawOffset(pawnData);
+
+                if (animal.jobs == null)
+                {
+                    animal.jobs = new Pawn_JobTracker(animal);
+                }
+
+                Job jobAnimal = new Job(BM_JobDefOf.Mounted_BattleMount, pawn);
+                jobAnimal.count = 1;
+                animal.jobs.TryTakeOrderedJob(jobAnimal);
+
+            }
+
+            //Pawn pawn = PawnGenerator.GeneratePawn(pawnKindDef, null);
+            //pawn.apparel = new Pawn_ApparelTracker(pawn);//have to add appareltracker to prevent null reference execption. 
+            //outPawns.Add(pawn);
+
+        }
+        //TODO: refactor this, should be in core
+        private static bool isMountable(String animalName)
+        {
+            GiddyUpCore.AnimalRecord value;
+            bool found = GiddyUpCore.Base.animalSelecter.Value.InnerList.TryGetValue(animalName, out value);
+            if (found && value.isSelected)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
