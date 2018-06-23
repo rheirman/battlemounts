830c49d0025d095aa5c9859d4e3d0c11e08173de
Roel <roelheirman@gmail.com>
2018-06-23 19:08:08 +0200

    fixed enemy mounts not working in ambushes

--- Source/BattleMounts/Harmony/IncidentWorker_Ambush_EnemyFaction.cs
+++ Source/BattleMounts/Harmony/IncidentWorker_Ambush_EnemyFaction.cs
@@ -13,15 +13,9 @@ using Verse;
 
 namespace Battlemounts.Harmony
 {
-    [HarmonyPatch(typeof(RimWorld.IncidentWorker_Ambush_EnemyFaction), "GeneratePawns")]
+    [HarmonyPatch(typeof(IncidentWorker_Ambush_EnemyFaction), "DoExecute")]
     static class IncidentWorker_Ambush_EnemyFaction_GeneratePawns
     {
-        static void Postfix(IncidentParms parms, ref List<Pawn> __result)
-        {
-            EnemyMountUtility.mountAnimals(ref __result, parms);
-        }
-
-        /*
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var instructionsList = new List<CodeInstruction>(instructions);
@@ -33,7 +27,7 @@ namespace Battlemounts.Harmony
                 if (instructionsList[i].operand == AccessTools.Method(typeof(IncidentWorker_Ambush), "PostProcessGeneratedPawnsAfterSpawning")) //Identifier for which IL line to inject to
 
                 {
-                    yield return new CodeInstruction(OpCodes.Ldloca_S, 2);//load generated pawns as parameter
+                    yield return new CodeInstruction(OpCodes.Ldarga_S, 2);//load generated pawns as parameter
                     yield return new CodeInstruction(OpCodes.Ldarg_1);//load incidentparms as parameter
                     yield return new CodeInstruction(OpCodes.Call, typeof(EnemyMountUtility).GetMethod("mountAnimals"));//Injected code                                                                                                                         //yield return new CodeInstruction(OpCodes.Stloc_2);
                 }
@@ -41,6 +35,5 @@ namespace Battlemounts.Harmony
             }
 
         }
-        */
     }
 }
--- Source/BattleMounts/Utilities/EnemyMountUtility.cs
+++ Source/BattleMounts/Utilities/EnemyMountUtility.cs
@@ -17,10 +17,14 @@ namespace Battlemounts.Utilities
     {
         public static void mountAnimals(ref List<Pawn> list, IncidentParms parms)
         {
-            if (list.Count == 0 || !(parms.raidArrivalMode == PawnsArrivalModeDefOf.EdgeWalkIn) || (parms.raidStrategy != null && parms.raidStrategy.workerClass == typeof(RaidStrategyWorker_Siege)))
+            Log.Message("mountAnimals called");
+            if (list.Count == 0 || !(parms.raidArrivalMode == null || parms.raidArrivalMode == PawnsArrivalModeDefOf.EdgeWalkIn) || (parms.raidStrategy != null && parms.raidStrategy.workerClass == typeof(RaidStrategyWorker_Siege)))
             {
+                Log.Message("something wrong, miemie");
                 return;
             }
+            Log.Message("really adding animals now");
+            Log.Message("list size: " + list.Count());
             NPCMountUtility.generateMounts(ref list, parms, Base.inBiomeWeight, Base.outBiomeWeight, Base.nonWildWeight, Base.enemyMountChance, Base.enemyMountChanceTribal);
             
             foreach(Pawn pawn in list)
