d4c883b1ba1d045d10ac1976ef1ae1219bc71cfd
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-02-26 13:31:58 +0100

    v1.1 Update

---
+++ v1.1/Source/BattleMounts/Harmony/Jobdriver_Cleanup.cs
@@ -0,0 +1,37 @@
+﻿using GiddyUpCore.Jobs;
+using GiddyUpCore.Storage;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+namespace BattleMounts.Harmony
+{
+    class Jobdriver_Cleanup
+    {
+        [HarmonyPatch(typeof(JobDriver), "Cleanup")]
+        static class JobDriver_Cleanup
+        {
+            static void Prefix(JobDriver __instance)
+            {
+                if(__instance.job.def != GUC_JobDefOf.Mounted)
+                {
+                    return;
+                }
+                JobDriver_Mounted jobDriver = (JobDriver_Mounted) __instance;
+
+                ExtendedPawnData pawnData = Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(jobDriver.pawn);
+                if (pawnData != null)
+                {
+                    Pawn Rider = jobDriver.Rider;
+                    ExtendedPawnData riderData = Base.Instance.GetExtendedDataStorage().GetExtendedDataFor(Rider);
+                    riderData.reset();
+                    jobDriver.pawn.Drawer.tweener = new PawnTweener(jobDriver.pawn);
+                }
+            }
+
+        }
+    }
+}
---
+++ v1.1/Source/BattleMounts/Harmony/Pawn_JobTracker.cs
@@ -0,0 +1,33 @@
+﻿using GiddyUpCore.Jobs;
+using GiddyUpCore.Storage;
+using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+
+namespace BattleMounts.Harmony
+{
+    [HarmonyPatch(typeof(Pawn_JobTracker), "StartJob")]
+    static class Pawn_JobTracker_StartJob
+    {
+
+        /*
+        static bool Prefix(Pawn_JobTracker __instance)
+        {
+            if (__instance.curDriver != null && __instance.curDriver.pawn != null && __instance.curDriver.pawn.CurJob != null && __instance.curDriver.pawn.CurJob.def == GUC_JobDefOf.Mounted)
+            {
+                return false;
+            }
+            return true;
+        }
+        */
+    }
+
+}
+
+
